eb34ec5fa6e45d80f009c19316e0620a46615629
linjiexin <770471223@qq.com>
2020-12-03 21:10:50 +0800

    嵌套路由，页面内转跳

--- src/router/index.js
+++ src/router/index.js
@@ -2,18 +2,34 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import login from '../views/login.vue'
+
+import EditPost from '../views/EditPost.vue'
+import PostList from '../views/PostList.vue'
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    children: [
+      {
+        path: 'postlist',
+        component: PostList
+      },
+      {
+        path: 'editpost',
+        component: EditPost
+      }
+    ]
+
   },
   {
     path: '/login',
     component: login
-  }
+  },
+
+
 
 ]
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,31 @@
 <template>
   <el-container class="container">
-    <el-aside width="200px">侧边栏</el-aside>
+    <el-aside width="200px">
+      <h2>黑马头条</h2>
+
+      <!-- 侧边栏 -->
+      <el-menu
+        class="el-menu-vertical-demo"
+        background-color="#000000"
+        text-color="#fff"
+        active-text-color="#ffd04b"
+      >
+        <el-menu-item index="1" @click="$router.push('/postlist')">
+          <i class="el-icon-menu"></i>
+          <span slot="title">导航二</span>
+        </el-menu-item>
+        <el-menu-item index="2" @click="$router.push('/editpost')">
+          <i class="el-icon-setting"></i>
+          <span slot="title">导航四</span>
+        </el-menu-item>
+      </el-menu>
+    </el-aside>
 
     <el-container>
       <el-header>头部内容</el-header>
-      <el-main>主要内容</el-main>
+      <el-main>
+        <router-view></router-view>
+      </el-main>
     </el-container>
   </el-container>
 </template>
@@ -19,17 +40,17 @@ export default {};
 }
 .el-header,
 .el-footer {
-  background-color: #b3c0d1;
+  background-color: #333333;
   color: #333;
   text-align: center;
   line-height: 60px;
 }
 
 .el-aside {
-  background-color: #333;
-  color: #333;
+  background-color: #000000;
+  color: #fff;
   text-align: center;
-  line-height: 200px;
+  line-height: 60px;
 }
 
 .el-main {
@@ -43,12 +64,16 @@ body > .el-container {
   margin-bottom: 40px;
 }
 
-.el-container:nth-child(5) .el-aside,
-.el-container:nth-child(6) .el-aside {
-  line-height: 260px;
-}
+// .el-container:nth-child(5) .el-aside,
+// .el-container:nth-child(6) .el-aside {
+//   line-height: 260px;
+// }
 
-.el-container:nth-child(7) .el-aside {
-  line-height: 320px;
+// .el-container:nth-child(7) .el-aside {
+//   line-height: 320px;
+// }
+
+.el-menu {
+  border-right: none;
 }
 </style>
