52bff8cab0cf0c1ca99345de6b37a5233c018726
linjiexin <770471223@qq.com>
2020-12-03 19:56:35 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+//引入axios
+import axios from 'axios'
+axios.defaults.baseURL = 'http://liangwei.tech:3000'
+Vue.prototype.$axios = axios
+//饿了么ui
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
