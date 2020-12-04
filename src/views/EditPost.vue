8be97ec9f48dcb1120e965bf586299fdd94d2332
linjiexin <770471223@qq.com>
2020-12-04 16:29:55 +0800

    图片上传功能

--- src/views/EditPost.vue
+++ src/views/EditPost.vue
@@ -1,9 +1,37 @@
 <template>
-  <h1>这是设置页</h1>
+  <div>
+    <!-- 请求已被封装在组件内，要cation发送链接，要加入公共链接 -->
+    <!-- header要加入请求头 设置一个token变量以便直接加入到组件中-->
+    <el-upload
+      :action="$axios.defaults.baseURL + '/upload'"
+      :headers="{
+                Authorization: token,
+            }"
+      :on-success="coverSuccess"
+      :on-remove="coverRemove"
+      list-type="picture-card"
+    >
+      <i class="el-icon-plus"></i>
+    </el-upload>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      token: localStorage.getItem("token"),
+    };
+  },
+  methods: {
+    coverSuccess(res, file, fileList) {
+      console.log("上传成功");
+    },
+    coverRemove(file, fileList) {
+      console.log("删除成功");
+    },
+  },
+};
 </script>
 
 <style>
